79717ef03f86b87cd1d727d02d2715a58d6b35b1
Khun Aung Paing <54024040+KhunMarana@users.noreply.github.com>
2020-12-02 18:16:36 +0630

    Update InchesToFeetConverter.java

--- src/com/khunaungpaing/ass/InchesToFeetConverter.java
+++ src/com/khunaungpaing/ass/InchesToFeetConverter.java
@@ -2,15 +2,10 @@ package com.khunaungpaing.ass;
 
 public class InchesToFeetConverter {
 
-	public void converter (double input){
-		int feet=0;
-		double inches = input;
+	public void converter (int input){
+		int feet = input/12;
+		int inches = input-(feet*12);
 		
-		while(inches>=12)
-		{
-			feet +=1;
-			inches-=12;
-		}
 	
 		System.out.println(feet+"\' "+inches+"\"");
 	}
